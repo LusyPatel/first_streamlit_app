1c898f9717cb0aa339823e195a1678a45ab0fd0b
LusyPatel <123729870+LusyPatel@users.noreply.github.com>
2023-01-27 16:05:43 +0530

    Update streamlit_app.py
    imported pandas liberary
    using the link given in the description we read the file using pandas.read_
    csv
    and using streamlit.dataframe() command we make a dataframe of the givn data on the link.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
 streamlit.text('🍥Hard boiled Free Range eggs')
 streamlit.text('🍞🥝Avacado Toast')
 streamlit.header('🍇🥝🍋🍊🍉🍏🍐Build Your Own Fruite Smoothie🍑🍒🍅🍓🥑🥭🍍🍌')
+import pandas
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
