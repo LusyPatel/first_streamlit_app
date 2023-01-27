c67c7b45aa95e709192736da4656e9bb2652468b
LusyPatel <123729870+LusyPatel@users.noreply.github.com>
2023-01-27 16:16:42 +0530

    Update streamlit_app.py
    since the last code we wrote did not made sense as it selected the numbers from the data so we have written  [my_fruit_list=my_fruit_list.set_index('Fruits')]
    This code will choose the fruit name directly and it will be more clearer to understand.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.header('🍇🥝🍋🍊🍉🍏🍐Build Your Own Fruite Smoothie🍑
 import pandas
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
-
+my_fruit_list=my_fruit_list.set_index('Fruits')
 # lets put a picklist for the customerts so they can pick the fruit of their choice in the smoothie.
 streamlit.multiselect('pick some fruit from this list:', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
