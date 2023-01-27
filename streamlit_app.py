80d837871a70b7a8c853714fc82785c8a3acbacf
LusyPatel <123729870+LusyPatel@users.noreply.github.com>
2023-01-27 15:49:15 +0530

    Update streamlit_app.py
    There was a typo error , I just fixed it from streanlit to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
-streanlit.header('Breakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard boiled Free Range eggs')
